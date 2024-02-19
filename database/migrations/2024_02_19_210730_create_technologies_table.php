91c0faaa006b140ff38be2913d521822a612e0b8
raffaelepiscitelli <raffaelepiscitelli1994@gmail.com>
2024-02-19 22:28:05 +0100

    Implementazione Seeder Technology

--- database/migrations/2024_02_19_210730_create_technologies_table.php
+++ database/migrations/2024_02_19_210730_create_technologies_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('technologies', function (Blueprint $table) {
             $table->id();
             $table->string('technology');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Technology;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,6 +13,18 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = [
+            'HTML',
+            'CSS',
+            'JavaScript',
+            'PHP',
+            'Laravel 10'
+        ];
+
+        foreach ($technologies as $singleTechnology) {
+            $newTechnology = new Technology();
+            $newTechnology->technology = $singleTechnology;
+            $newTechnology->save();
+        }
     }
 }
